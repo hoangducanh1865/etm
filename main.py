7be5daf7837a4386e62eb231c3d43f6181f2837d
hoangducanh1865 <hoangducanh.1865@gmail.com>
2025-09-13 22:48:19 +0700

    Add dataset preprocessor, raw datasets, stopword list

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 def main():
     pass
-    
+
 
 if __name__ == "__main__":
     main()
--- src/config/config.py
+++ src/config/config.py
@@ -1,5 +1,9 @@
 import torch
 
 
-class Config
+class Config:
     DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+    
+    STOPWORDS_FILE_PATH = "data/raw_data/stopwords.txt"
+    OUTPUT_DIR = "data/preprocessed_data/"
+    NYT_PATH = "data/raw_data/nyt.txt"
